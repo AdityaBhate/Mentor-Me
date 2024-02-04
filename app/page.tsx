3180f398674eb4d0a94209ea7ab1cfc6602975cf
Aditya Bhate <adityabhate305@gmail.com>
2024-02-04 12:35:17 +0530

    authentication setup

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-	return (
-		<p className='text-center text-3xl p-4 font-bold'>
-			<Button>Click Me!</Button>
-		</p>
-	);
-}
